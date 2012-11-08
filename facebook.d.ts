43de5d224c30db5d5bddf1f571a0e0e695ff8396
Mient-jan Stelling <mientjan.stelling@gmail.com>
2012-11-08 23:34:20 +0100

    added some extra comments, added hideFlashCallback method declaration

--- facebook.d.ts
+++ facebook.d.ts
@@ -8,15 +8,25 @@ license: [MIT License](https://github.com/mientjan/typescript-facebook-definitio
 */
 
 interface FBUserAuthenticate {
-	status: string;				// Current status of the session
+	// Current status of the session
+	status: string;	// connected or not_authorized
+	
 	authResponse?: FBAuthResponse;
 }
 
 interface FBAuthResponse {
-	userID: string;         		// String representing the current user's ID 
-	signedRequest: string;  		// String with the current signedRequest 
-	expiresIn: string;			// UNIX time when the session expires
-	accessToken: string;			// Access token of the user 
+
+	// String representing the current user's ID 
+	userID: string;
+	
+	// String with the current signedRequest     		
+	signedRequest: string;
+	
+	// UNIX time when the session expires	
+	expiresIn: string;
+
+	// Access token of the user 
+	accessToken: string;
 }
 
 interface FBUIParameters {
@@ -30,15 +40,51 @@ interface FBUIParameters {
 
 
 interface FBInitParameters {
-	appId?: string;			// Your application ID. Default null
-	cookie?:bool;			// true to enable cookie support. false
-	logging?: bool;			// false to disable logging.Optional	true
-	status?: bool;			// true to fetch fresh status.Optional	true
-	xfbml?: bool;			// true to parse XFBML tags.Optional	false
-	channelUrl?: string;		// Specifies the URL of a custom URL channel file.This file must contain a single script element pointing to the JavaScript SDK URL.Optional	null
-	authResponse?: Object;		// Manually set the object retrievable from getAuthResponse.Optional	true
-	frictionlessRequests?: bool;	//  see Frictionless Requests	Optional	false
-	hideFlashCallback?: Object;	// see Custom Flash Hide Callback	Optional	null
+	// Your application ID.
+	appId?: string;	// default null	
+
+	// true to enable cookie support.
+	cookie?:bool; // default false			
+
+	// false to disable logging.
+	logging?: bool;	// default true			
+	
+	// true to fetch fresh status.
+	status?: bool; // default true
+	
+	// true to parse XFBML tags.
+	xfbml?: bool; // default false
+	
+	// Specifies the URL of a custom URL channel file. This file must contain a single 
+	// script element pointing to the JavaScript SDK URL.
+	channelUrl?: string; // default true
+
+	// Manually set the object retrievable from getAuthResponse.
+	authResponse?: Object; // default true
+	
+	/**
+	 * Frictionless Requests enable users to send Requests to specific friends from within 
+	 * an app without having to click on a pop-up confirmation dialog. Upon sending a Request 
+	 * to a friend from within an app, a user may authorize the app to send subsequent Requests 
+	 * to the same friend without a Dialog prompt. This removes a Dialog from the flow and 
+	 * streamlines the process of sharing with friends.
+	 * 
+	 * @see http://developers.facebook.com/docs/reference/dialogs/requests/#frictionless_requests
+	 */
+	frictionlessRequests?: bool; // default false
+
+	/**
+	 * Developers who wish to provide a custom hide and display experience may pass a 
+	 * JavaScript function in the hideFlashCallback option for FB.init. This function 
+	 * will be executed whenever the Flash object is hidden or displayed due to user 
+	 * behavior (clicking on a Notification, etc.) and can be used by a developer to 
+	 * take the appropriate actions: hiding or displaying their Flash object. It receives 
+	 * a parameter of type object that contains two properties
+	 */
+	hideFlashCallback?: (params: { 
+		state: string; // 'opened' or 'closed'
+		elem: HTMLElement; 
+	}) => {};	
 }
 
 interface FB {
@@ -63,11 +109,13 @@ interface FB {
 	): void;
 
 	// FB.logout will log the user out of both your site and Facebook. 
-	//	You will need to have a valid access token for the user in order to call the function.
+	// You will need to have a valid access token for the user in order to call the function.
 	logout(cb?:(response?:Object) => any );
 
 	getLoginStatus(
 		cb?:(response:FBUserAuthenticate) => void, 
+
+		// Access token of the user 
 		force?:bool
 	):void;
 
@@ -79,6 +127,8 @@ interface FB {
 		 * FB.Events.subscribe
 		 * 
 		 * subscribe gives different response objects depending on the subscribed event.
+		 * 
+		 * @see http://developers.facebook.com/docs/reference/javascript/FB.Event.subscribe/
 		 */
 
 		/**
@@ -123,6 +173,9 @@ interface FB {
 		 */
 		subscribe ( name: string, cb: (response: string ) => any );
 
+		/**
+		 * @see http://developers.facebook.com/docs/reference/javascript/FB.Event.unsubscribe/
+		 */
 		unsubscribe ( name: string, cb: (response: string ) => any );
 	};
 
@@ -140,12 +193,29 @@ interface FB {
 		};
 
 		getPageInfo(cb: (info: {
-			clientHeight: number;	// The height of the viewport in pixels
-			clientWidth: number;	// The width of the viewport in pixels
-			offsetLeft: number;	// The number of pixels between the left edge of the viewport and the left edge of your app's iframe
-			offsetTop: number;	// The number of pixels between the top edge of the viewport and the top edge of your app's iframe
-			scrollLeft: number;	// The number of pixels between the left edge of your iframe and the left edge of your iframe's viewport
-			scrollTop: number;	// The number of pixels between the top edge of your iframe and the top edge of your iframe's viewport
+
+			// The height of the viewport in pixels
+			clientHeight: number;	
+
+			// The width of the viewport in pixels
+			clientWidth: number;	
+
+			// The number of pixels between the left edge of the viewport and 
+			// the left edge of your app's iframe
+			offsetLeft: number;
+
+			// The number of pixels between the top edge of the viewport and 
+			// the top edge of your app's iframe
+			offsetTop: number;
+
+			// The number of pixels between the left edge of your iframe and 
+			// the left edge of your iframe's viewport
+			scrollLeft: number;
+			
+			// The number of pixels between the top edge of your iframe and 
+			// the top edge of your iframe's viewport	
+			scrollTop: number;
+
 		}) => void ): void;
 
 		hideFlashElement(elem:Element): void;
